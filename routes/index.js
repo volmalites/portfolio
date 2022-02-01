fa394a4bfa1f1531564bc3be0c66f64d0e7da8b0
Bjorn Lottering (Volmalites) <bjorn@octonet.co.za>
2022-02-01 08:23:29 +0200

    Added logs for error handling

--- routes/index.js
+++ routes/index.js
@@ -33,16 +33,20 @@ router.use((req, res, next) => {
   const err = new Error();
   err.status = 404;
   err.message = 'The requested page does not exist.';
+  res.statusMessage = err.message;
   res.status(404).render('page-not-found', { err });
+  console.log(`Error ${err.status}:`, err.message);
 });
 
 router.use((err, req, res, next) => {
+  res.statusMessage = err.message;
   if (err.status === 404) {
     res.status(404).render('page-not-found', { err });
   } else {
     err.message = err.message || 'Oops! It looks like something went wrong on the server.';
     res.status(err.status || 500).render('error', { err });
   }
+  console.log(`Error ${err.status}:`, err.message);
 });
 
 module.exports = router;
